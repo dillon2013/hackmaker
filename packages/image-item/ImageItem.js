6e81c23846cfedfd0dc8985d077e99e106ef15d0
Dillon Lee <dillon.lee@hostmaker.co>
2018-10-18 10:19:27 +0100

    draft 4

--- packages/image-item/ImageItem.js
+++ packages/image-item/ImageItem.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 function ImageItem({src}) {
     return (
         <div>
+            <span>hello</span>
             <img src={src} alt=""/>
         </div>
     )
